918c71328f4ecf48723bac41d269c17b3335637e
Marcelo <marcelegacy32@gmail.com>
2025-12-01 22:21:36 -0500

    Rutas

---
+++ Routes/userRoutes.js
@@ -0,0 +1,37 @@
+import { Controller } from "../Controllers/userController";
+const express=require("express");
+const router=express.Router();
+
+/*Esto garantiza que solo exista una instancia del controlador y del arreglo de datos*/
+const controller=new Controller();
+
+
+router.get("/users", (req, res)=>{
+    
+    try
+    {
+        const users = controller.getAllUsers();
+        res.status(200).json(users);
+    }
+    catch(error)
+    {
+        res.status(500).json({message: error.message});
+    }
+});
+
+router.post("/users/new", (req, res)=>{
+    
+    try
+    {
+        const name=req.body.name;
+        const email=req.body.email;
+        const response=controller.addNewUser(name, email);
+        res.status(201).json(response.message);
+    }
+    catch(error)
+    {
+        res.status(500).json({message: error.message});
+    }
+});
+
+module.exports=router;
