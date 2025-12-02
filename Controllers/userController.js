790ea0c00eef82352e9b8ff107840588c1135e2f
Marcelo <marcelegacy32@gmail.com>
2025-12-01 21:54:48 -0500

    Estructura inicial

---
+++ Controllers/userController.js
@@ -0,0 +1,39 @@
+import { User } from "../Models/User";
+
+export class Controller{
+    constructor()
+    {
+        this.database=[];
+    }
+
+    getAllUsers() {
+        return JSON.stringify(this.database);
+    }
+
+    addNewUser(name, email){
+        if(this.validateName(name) && this.validateMail(email))
+        {
+            let newId=this.database.length+1;
+            this.database.push(new User(newId, name, email));
+        }
+        else
+        {
+            throw new Error("Error. Nombre o correo invalidos, no se pudo guardar.");
+        }
+    }
+
+    validateName(name){
+        const nameRegexp=/^([A-Za-zñÑ]){3,20}$/;
+        /*Esta expresión regular valida que:
+        - Solo se ingresen letras mayusculas y minusculas, incluida la ñ
+        - Se ingrese como minimo 3 letras y como maximo 20*/
+        return nameRegexp.test(name);
+    }
+
+    validateMail(email)
+    {
+        const emailRegexp=/^[a-zA-Z0–9._%+-]+@[a-zA-Z0–9.-]+\.[a-zA-Z]{2,}$/;
+
+        return emailRegexp.test(email);
+    }
+}
